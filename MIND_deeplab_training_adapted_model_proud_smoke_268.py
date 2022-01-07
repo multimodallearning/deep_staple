783e081c335d15bfc33f51173e7a5c178e12fc1b
Christian Weihsbach <christian.weihsbach@uni-luebeck.de>
2022-01-07 15:45:52 +0100

    Remove normalizing with t_metric

--- MIND_deeplab_training_adapted_model_proud_smoke_268.py
+++ MIND_deeplab_training_adapted_model_proud_smoke_268.py
@@ -1655,9 +1655,9 @@ def train_DL(run_name, config, training_dataset):
 
                         bare_weight = embedding(b_idxs_dataset).squeeze()
                         # t_metric = (gt_num.cuda()[b_idxs_dataset]+np.exp(1)).log()+np.exp(1)
-                        t_metric = (gt_num.cuda()[b_idxs_dataset]+1.).log()+1.
+                        # t_metric = (gt_num.cuda()[b_idxs_dataset]+1.).log()+1.
                         weight = torch.sigmoid(bare_weight)
-                        weight = weight/weight.mean()/t_metric
+                        weight = weight/weight.mean()
 
                         # Prepare logits for scoring
                         logits_for_score = logits.argmax(1)
