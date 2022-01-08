494c8d0efb74ef0e641e540aa44cef574eb7aace
Christian Weihsbach <christian.weihsbach@uni-luebeck.de>
2022-01-08 02:25:11 +0100

    Change onset of fear to begin after epoch 0

--- MIND_deeplab_training_adapted_model_proud_smoke_268.py
+++ MIND_deeplab_training_adapted_model_proud_smoke_268.py
@@ -1708,7 +1708,7 @@ def train_DL(run_name, config, training_dataset):
                             risk_regularization = -weight*p_pred_num/(logits_for_score.shape[-2]*logits_for_score.shape[-1])
                             regularization = regularization + risk_regularization.sum()
 
-                        if epx > 3 and True:
+                        if epx > 0 and True:
                             with torch.no_grad():
                                 grads = embedding_grad_storage[b_idxs_dataset]
                                 grads_grads = F.conv1d(grads.unsqueeze(1), weight=torch.tensor([[[-0.5,0.0,0.5]]]).to(grads), padding='same')
