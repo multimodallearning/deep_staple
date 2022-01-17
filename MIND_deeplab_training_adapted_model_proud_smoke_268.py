5b852821964a398ed9a30dd915fb44fee772c3f7
Christian Weihsbach <christian.weihsbach@uni-luebeck.de>
2022-01-17 19:12:20 +0100

    Add delayed onset of data parameter optimizer

--- MIND_deeplab_training_adapted_model_proud_smoke_268.py
+++ MIND_deeplab_training_adapted_model_proud_smoke_268.py
@@ -1006,7 +1006,7 @@ def train_DL(run_name, config, training_dataset):
                 scaler.scale(loss).backward()
                 scaler.step(optimizer)
 
-                if str(config.data_param_mode) != str(DataParamMode.DISABLED):
+                if str(config.data_param_mode) != str(DataParamMode.DISABLED) and epx > 2:
                     scaler.step(optimizer_dp)
 
                 scaler.update()
